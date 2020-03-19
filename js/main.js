c5dc1eec355443b5b0d391b84dd65e63fd51b97b
shane-sys <shanepatrickgraham@gmail.com>
2020-03-19 12:56:59 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//alert('yo')
+document.querySelector('button').innerHTML
+document.querySelector('click', =>{
+let player = 1
+let bot = 2
+
+})
+
+
+class player1 {
+
+  constructor(player1) {
+    this.name = player1;
+    this.name = bot1;
+    this.match = => (){
+
+    }
+  }
+
+
+  thisMatch() {
+    console.log(this.name);
+    nextOne(this.player1,this.bot2)
+    functionNextfunc(a,b);
+
+  }
+
+}
---
+++ nodeModuels/server.js
@@ -0,0 +1,5 @@
+const http
+const fs
+const url
+const querystring
+const figlet
